6e99abf692ba9b55498839baed90ec55f94a1977
uday1921 <37540454+uday1921@users.noreply.github.com>
2021-05-27 16:30:57 +0530

    Update approval.groovy

--- approval.groovy
+++ approval.groovy
@@ -2,8 +2,12 @@ import org.ini4j.Ini
 
 def approvalStage()
 {
-    sendmail()
-    def submitter  = input(message: "would you like to provide approval")
+   // sendmail()
+    //def submitter  = input(message: "would you like to provide approval")
+    name = creds("details", "name").toString().trim()
+    sur = creds("details", "surname").toString().trim()
+    echo "${name}" 
+    echo "${sur}"
     
 }
 def sendmail()
@@ -13,5 +17,10 @@ def sendmail()
     to: 'udaykumar.gorrepati123@gmail.com',
     mimeType: 'text/html',
     body: "goto ${BUILD_URL}  to approve stahe"
+}
+def creds(key,value)
+{
+    def detailcreds = load Ini(new FileInputStream(".connect_param.cnf.txt"));
+    return detailcreds.get(key,value);
 }
 return this
